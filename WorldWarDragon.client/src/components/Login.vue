1970aef56b19a1927b6830e9292f11a3110ba9bd
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-07-08 02:42:43 -0600

    Officially able to log in to World War Dragon!

--- WorldWarDragon.client/src/components/Login.vue
+++ WorldWarDragon.client/src/components/Login.vue
@@ -1,30 +1,15 @@
-<script setup>
-import { computed } from 'vue';
-import { AppState } from '../AppState';
-import { AuthService } from '../services/AuthService';
-
-const identity = computed(() => AppState.identity)
-const account = computed(() => AppState.account)
-async function login() {
-  AuthService.loginWithPopup()
-}
-async function logout() {
-  AuthService.logout()
-}
-
-</script>
-
 <template>
   <span class="navbar-text">
-    <button class="btn selectable text-success lighten-30 text-uppercase my-2 my-lg-0" @click="login" v-if="!identity">
+    <button class="btn selectable text-success lighten-30 text-uppercase my-2 my-lg-0" @click="login"
+      v-if="!user.isAuthenticated">
       Login
     </button>
     <div v-else>
       <div class="dropdown my-2 my-lg-0">
         <div type="button" class="bg-dark border-0 selectable no-select" data-bs-toggle="dropdown"
           aria-expanded="false">
-          <div v-if="account?.picture || identity?.picture">
-            <img :src="account?.picture || identity?.picture" alt="account photo" height="40" class="rounded" />
+          <div v-if="account.picture || user.picture">
+            <img :src="account.picture || user.picture" alt="account photo" height="40" class="rounded" />
           </div>
         </div>
         <div class="dropdown-menu dropdown-menu-sm-end dropdown-menu-start p-0" aria-labelledby="authDropdown">
@@ -44,5 +29,25 @@ async function logout() {
     </div>
   </span>
 </template>
+
+<script>
+import { computed } from 'vue'
+import { AppState } from '../AppState'
+import { AuthService } from '../services/AuthService'
+export default {
+  setup() {
+    return {
+      user: computed(() => AppState.user),
+      account: computed(() => AppState.account),
+      async login() {
+        AuthService.loginWithPopup()
+      },
+      async logout() {
+        AuthService.logout({ returnTo: window.location.origin })
+      }
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped></style>
