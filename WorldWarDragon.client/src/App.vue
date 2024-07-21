8b2ac4798d0f60b26237b2e4615fa1d5ecdd0059
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-07-21 02:36:41 -0600

    Removed old comment

--- WorldWarDragon.client/src/App.vue
+++ WorldWarDragon.client/src/App.vue
@@ -3,26 +3,6 @@ import { RouterView, useRoute } from 'vue-router'
 import NavBar from './components/Navbar.vue'
 import OffBar from './components/OffBar.vue'
 import { onMounted, ref, watchEffect } from "vue";
-// const route = useRoute()
-// const bgImg = ref()
-// bgImg.value = '../public/assets/towerbg4.jpeg'
-
-// onMounted(() => {
-//     setBgImg(route)
-
-// })
-
-// // watchEffect(() => {
-// //     setBgImg(route)
-// // })
-
-// const setBgImg = () => {
-//     if (route.name === 'Home') {
-//         bgImg.value = '../public/assets/towerbg4.jpeg';
-//     } else if (route.name === 'Score') {
-//         bgImg.value = '../public/assets/dragonCave2.jpeg';
-//     }
-// };
 
 </script>
 
