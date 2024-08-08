5a62b0b9373b1908c00207ff9e64813bb5efefb5
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-08-08 02:59:52 -0600

    changed account background image

--- WorldWarDragon.client/src/components/Login.vue
+++ WorldWarDragon.client/src/components/Login.vue
@@ -47,6 +47,7 @@ export default {
       async logout() {
         AuthService.logout()
       }
+
     }
   }
 }
--- WorldWarDragon.client/src/pages/AccountPage.vue
+++ WorldWarDragon.client/src/pages/AccountPage.vue
@@ -157,7 +157,7 @@ export default {
     const setBgImg = () => {
       const mainElement = document.querySelector('main');
       if (mainElement) {
-        let bgImg = '/assets/scales.jpeg';
+        let bgImg = '/assets/chamber.jpeg';
         mainElement.style.backgroundImage = `linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.90) 100%), url(${bgImg})`;
       }
     }
@@ -223,7 +223,6 @@ export default {
         } else {
           levelMode.value = false
           this.levelNew = 0
-          // this.levelUp = [0, 0]
         }
       },
       selectPicture(pictureId) {
@@ -245,10 +244,6 @@ export default {
 
 
 <style lang="scss" scoped>
-.custom-bg {
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='28' height='49' viewBox='0 0 28 49'%3E%3Cg fill-rule='evenodd'%3E%3Cg id='hexagons' fill='%239C92AC' fill-opacity='0.25' fill-rule='nonzero'%3E%3Cpath d='M13.99 9.25l13 7.5v15l-13 7.5L1 31.75v-15l12.99-7.5zM3 17.9v12.7l10.99 6.34 11-6.35V17.9l-11-6.34L3 17.9zM0 15l12.98-7.5V0h-2v6.35L0 12.69v2.3zm0 18.5L12.98 41v8h-2v-6.85L0 35.81v-2.3zM15 0v7.5L27.99 15H28v-2.31h-.01L17 6.35V0h-2zm0 49v-8l12.99-7.5H28v2.31h-.01L17 42.15V49h-2z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E"), linear-gradient(to right top, #343a40, #2b2c31, #211f22, #151314, #000000);
-}
-
 .character-container {
   background-color: var(--bs-body-bg);
   border: solid 4px var(--bs-outline);
