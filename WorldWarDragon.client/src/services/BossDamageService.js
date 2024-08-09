c8ad8a9c744abc376ca096e62c62bd2326c6d1f3
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-08-09 08:02:49 -0600

    Cleaned up duplicate get requests

--- WorldWarDragon.client/src/services/BossDamageService.js
+++ WorldWarDragon.client/src/services/BossDamageService.js
@@ -30,14 +30,5 @@ class BossDamageService {
     return res.data
   }
 
-  // async updateBossDamageByBossId(bossDamageData) {
-  //   await api.put(`api/bossDamage/${bossDamageData.bossId}`, bossDamageData)
-  //   AppState.activeBoss.hp -= bossDamageData.dmg
-  // }
-
-  // async removeBossDamageById(bossDamageId) {
-  //   await api.delete(`api/bossDamage/${bossDamageId}`)
-  // }
-
 }
 export const bossDamageService = new BossDamageService()
